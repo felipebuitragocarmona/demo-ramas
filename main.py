c2208d76ac4a3a3aa75c2d93c976e421b9a25615
felipebuitragocarmona <felipebuitragocarmona@gmail.com>
2026-03-06 08:20:23 -0500

    ya vuelvo

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 print("Hello, World! Version 1.0")
 print("Esto ya está funcionado")
+
+# HU-1
+# Función para verificar si una cadena es un palíndromo
+
+def palindromo(s):
+    s = s.replace(" ", "").lower()
+    return s == s[::-1]
+
+print(palindromo("Ana"))  # True
+print(palindromo("Hola"))  # False
